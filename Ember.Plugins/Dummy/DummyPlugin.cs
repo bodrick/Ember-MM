72e3cb01448d2079f9a685f2c335b17874fa59cf
Andrew Fyfe <andrew@neptune-one.net>
2012-09-05 23:58:16 +0100

    Ember.Plugins: Add a dummy plug-in for debugging.
    Signed-off-by: Andrew Fyfe <andrew@neptune-one.net>

---
+++ Ember.Plugins/Dummy/DummyPlugin.cs
@@ -0,0 +1,68 @@
+﻿using Ember.Plugins.Scraper;
+
+namespace Ember.Plugins.Dummy
+{
+    class DummyPlugin
+        : PluginBase
+    {
+
+        #region Fields
+
+        private static readonly log4net.ILog log = log4net.LogManager.GetLogger(
+            System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
+        #endregion
+
+
+        #region IPlugin
+
+        #region Properties
+
+        /// <summary>
+        /// The name of the plug-in.
+        /// </summary>
+        public override string Name
+        {
+            get { return "A Dummy Plug-in"; }
+        }
+
+        #endregion Properties
+
+
+        #region Methods
+
+        public override void InitPlugin(PluginManager manager)
+        {
+            base.InitPlugin(manager);
+
+            manager.MovieScraper.PreMovieInfoScrape += PreMovieInfoScraperAction;
+            manager.MovieScraper.PostMovieInfoScrape += PostMovieInfoScraperAction;
+        }
+
+        #endregion Methods
+
+        #endregion IPlugin
+
+
+        #region Event Handlers
+
+        private MovieInfoScraperActionContext PreMovieInfoScraperAction(
+            MovieInfoScraperActionContext context)
+        {
+            if (log.IsDebugEnabled)
+                log.Debug("PreMovieInfoScraperAction[Dummy]");
+            return context;
+        }
+
+        private PluginActionResult PostMovieInfoScraperAction(
+            PluginActionResult result)
+        {
+            if (log.IsDebugEnabled)
+                log.Debug("PostMovieInfoScraperAction[Dummy]");
+            return result;
+        }
+
+        #endregion EventHandlers
+
+    }
+}
