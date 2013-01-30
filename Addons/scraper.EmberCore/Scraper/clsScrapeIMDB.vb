2e4c8733fe2a418e10ec6097de69b07d8da61171
DanCooper <stephan.schueck@gmail.com>
2013-01-30 10:44:48 +0100

    change autoscraping (test)

--- Addons/scraper.EmberCore/Scraper/clsScrapeIMDB.vb
+++ Addons/scraper.EmberCore/Scraper/clsScrapeIMDB.vb
@@ -678,7 +678,7 @@ mPlot:
                         Dim exactHaveYear As Integer = FindYear(dbMovie.Filename, r.ExactMatches)
                         Dim popularHaveYear As Integer = FindYear(dbMovie.Filename, r.PopularTitles)
                         'it seems "popular matches" is a better result than "exact matches"
-                        If r.ExactMatches.Count = 1 AndAlso r.PopularTitles.Count = 0 AndAlso r.PartialMatches.Count = 0 Then 'redirected to imdb info page
+                        If r.ExactMatches.Count = 1 Then ' AndAlso r.PopularTitles.Count = 0 AndAlso r.PartialMatches.Count = 0 Then 'redirected to imdb info page
                             b = GetMovieInfo(r.ExactMatches.Item(0).IMDBID, imdbMovie, Master.eSettings.FullCrew, Master.eSettings.FullCast, False, Options, True)
                         ElseIf (popularHaveYear >= 0 OrElse exactHaveYear = -1) AndAlso r.PopularTitles.Count > 0 AndAlso (r.PopularTitles(If(popularHaveYear >= 0, popularHaveYear, 0)).Lev <= 5 OrElse useAnyway) Then
                             b = GetMovieInfo(r.PopularTitles.Item(If(popularHaveYear >= 0, popularHaveYear, 0)).IMDBID, imdbMovie, Master.eSettings.FullCrew, Master.eSettings.FullCast, False, Options, True)
