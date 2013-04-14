9526681f9ac3486e3fab7a2a6c796a9e705db6b8
DanCooper <stephan.schueck@gmail.com>
2013-04-14 11:19:29 +0200

    changed: moved trakt.tv lang files to addons

--- Addons/multi.EmberExtras/frmTrakt.vb
+++ Addons/multi.EmberExtras/frmTrakt.vb
@@ -21,15 +21,15 @@ Public Class frmTrakt
         prgtrakt.Maximum = myWatchedMovies.Count
         prgtrakt.Minimum = 0
         prgtrakt.Step = 1
-        chkUseTrakt.Text = Master.eLang.GetString(870, "Use Trakt.tv", True)
-        btGetMoviesTrakt.Text = Master.eLang.GetString(872, "Get watched movies", True)
-        btSaveMoviesTrakt.Text = Master.eLang.GetString(873, "Save playcount to database/Nfo", True)
+        chkUseTrakt.Text = Master.eLang.GetString(68, "Use trakt.tv as source for ""Playcount""")
+        btGetMoviesTrakt.Text = Master.eLang.GetString(70, "Get watched movies")
+        btSaveMoviesTrakt.Text = Master.eLang.GetString(71, "Save playcount to database/Nfo")
         txtTraktUser.Text = Master.eSettings.TraktUser
-        lblTraktUser.Text = Master.eLang.GetString(875, "Username", True)
+        lblTraktUser.Text = Master.eLang.GetString(73, "Username")
         txtTraktPassword.Text = Master.eSettings.TraktPassword
-        lblTraktPassword.Text = Master.eLang.GetString(876, "Password", True)
-        btGetSeriesTrakt.Text = Master.eLang.GetString(877, "Get watched episodes", True)
-        btnSavetraktsettings.Text = Master.eLang.GetString(878, "Save", True)
+        lblTraktPassword.Text = Master.eLang.GetString(74, "Password")
+        btGetSeriesTrakt.Text = Master.eLang.GetString(75, "Get watched episodes")
+        btnSavetraktsettings.Text = Master.eLang.GetString(76, "Save")
         txtTraktPassword.PasswordChar = "*"c
 
         If Not String.IsNullOrEmpty(Master.eSettings.UseTrakt.ToString) Then
