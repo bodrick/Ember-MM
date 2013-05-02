04f1982eb3e7d8e1f8273d8afd2caf6c8cab741a
DanCooper <stephan.schueck@gmail.com>
2013-05-02 12:35:15 +0200

    release: 1.3.0.12
    - added changelog.txt

--- Addons/multi.EmberExtras/My Project/AssemblyInfo.vb
+++ Addons/multi.EmberExtras/My Project/AssemblyInfo.vb
@@ -27,5 +27,5 @@ Imports System.Runtime.InteropServices
 ' You can specify all the values or you can default the Build and Revision Numbers
 ' by using the '*' as shown below:
 ' <Assembly: AssemblyVersion("1.0.*")>
-<Assembly: AssemblyVersion("1.3.0.11")> 
-<Assembly: AssemblyFileVersion("1.3.0.11")> 
+<Assembly: AssemblyVersion("1.3.0.12")> 
+<Assembly: AssemblyFileVersion("1.3.0.12")> 
